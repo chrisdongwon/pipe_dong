e28e90e4dad08094b36367cb378c8e3caf1ec03d
Chris Won <chrisdongwon@gmail.com>
2025-01-09 15:24:17 +0700

    Update pipex_bonus.c

--- pipex_bonus.c
+++ pipex_bonus.c
@@ -12,6 +12,82 @@
 
 #include "pipex_bonus.h"
 
+// int prev_fd = -1;
+
+// // Open input file for reading
+// int input_fd = open(input_file, O_RDONLY);
+// if (input_fd < 0) {
+// perror("Failed to open input file");
+// exit(1);
+// }
+
+// // Open output file for writing
+// int output_fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+// if (output_fd < 0) {
+// perror("Failed to open output file");
+// close(input_fd);
+// exit(1);
+// }
+
+// for (int i = 0; i < num_commands; i++) {
+// if (i < num_commands - 1) {
+//     // Create a pipe for the next command
+//     if (pipe(pipe_fds) < 0) {
+// 	perror("Pipe failed");
+// 	exit(1);
+//     }
+// }
+
+// pid_t pid = fork();
+// if (pid < 0) {
+//     perror("Fork failed");
+//     exit(1);
+// }
+
+// if (pid == 0) { // Child process
+//     if (i == 0) {
+// 	// Redirect input for the first command
+// 	dup2(input_fd, STDIN_FILENO);
+// 	close(input_fd);
+//     } else {
+// 	// Redirect input to previous pipe
+// 	dup2(prev_fd, STDIN_FILENO);
+// 	close(prev_fd);
+//     }
+
+//     if (i < num_commands - 1) {
+// 	// Redirect output to the current pipe
+// 	dup2(pipe_fds[1], STDOUT_FILENO);
+// 	close(pipe_fds[0]);
+// 	close(pipe_fds[1]);
+//     } else {
+// 	// Redirect output to the output file
+// 	dup2(output_fd, STDOUT_FILENO);
+// 	close(output_fd);
+//     }
+
+//     // Execute the command
+//     char *cmd[] = {"/bin/sh", "-c", commands[i], NULL};
+//     execvp(cmd[0], cmd);
+
+//     // If execvp fails
+//     perror("Exec failed");
+//     exit(1);
+// } else { // Parent process
+//     if (i > 0) {
+// 	close(prev_fd);
+//     }
+
+//     if (i < num_commands - 1) {
+// 	close(pipe_fds[1]);
+// 	prev_fd = pipe_fds[0];
+//     }
+// }
+// }
+
+// close(input_fd);
+// close(output_fd);
+
 static void	exec_cmd(t_pipex *param, ssize_t i)
 {
 	if (!i)
